4515233fe945e1794082e0fbfad47335ca5f60bc
Frank <Frank@Frank>
2016-10-26 00:24:00 +0900

    -Add Properties File with information to connect to the Database - Add connection and disconnection features to the HiltonDublinDBConnection

--- hiltonDublin/src/com/hiltondublin/db/HiltonDublinDBConnection.java
+++ hiltonDublin/src/com/hiltondublin/db/HiltonDublinDBConnection.java
@@ -1,5 +1,130 @@
 package com.hiltondublin.db;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class HiltonDublinDBConnection {
+	public final static String DB_PROPERTIES_FILENAME = "db.properties";
+	public final static String HILTONDUBLIN_DB_DRIVER = "hiltondublin.db.driver";
+	public final static String HILTONDUBLIN_DB_URL = "hiltondublin.db.url";
+	public final static String HILTONDUBLIN_DB_USERNAME = "hiltondublin.db.username";
+	public final static String HILTONDUBLIN_DB_PASSWORD = "hiltondublin.db.password";
+	public final static String HILTONDUBLIN_DB_NAME = "hiltondublin.db.name";
+	
+	private static HiltonDublinDBConnection instance;
+	private Connection dbConnection;
+	private Properties dbProperties;
+	
+	/**
+	 * Returns the instance of the HiltonDublinDBConnection (Singleton)
+	 * @return HiltonDublinDBConnection
+	 */
+	public static HiltonDublinDBConnection getHiltonDublinDBConnectionInstance(){
+		if(HiltonDublinDBConnection.instance == null){
+			instance = new HiltonDublinDBConnection();
+		}
+		return HiltonDublinDBConnection.instance;
+	}
+	
+	/**
+	 * Load Properties and Connect to the DB
+	 */
+	private HiltonDublinDBConnection(){
+		//Load Properties
+		if(loadDBProperties()){
+			System.out.println("Successfully load Properties from file \"" + DB_PROPERTIES_FILENAME + "\"");
+		} else {
+			System.out.println("Failed to load Properties from file \"" + DB_PROPERTIES_FILENAME + "\"");
+		}
+		
+		//Connect to Database
+		if(connect()){
+			System.out.println("Successfully connect to Database \"" + dbProperties.getProperty(HILTONDUBLIN_DB_NAME) + "\"");
+		} else {
+			System.out.println("Failed to connect to Database \"" + dbProperties.getProperty(HILTONDUBLIN_DB_NAME) + "\"");
+		}
+	}
+	
+	
+	/**
+	 * Load the all for the database connection necessary properties from the file DB_PROPERTIES_FILENAME and saves it in the attribute dbProperties
+	 * @return boolean
+	 * true if propertie load was successful
+	 * false if propertie load failed
+	 */
+	public boolean loadDBProperties() {
+		System.out.println("Load \"" + DB_PROPERTIES_FILENAME + "\"");
+		InputStream input = null;
+		input = getClass().getClassLoader().getResourceAsStream(DB_PROPERTIES_FILENAME);
+		try {
+			this.dbProperties.load(input);
+		} catch (IOException e) {
+			System.out.println("Failed to load \"" + DB_PROPERTIES_FILENAME + "\"");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Connects to the database with the properties from the propertie file
+	 * @return boolean
+	 * true if connection was successful
+	 * false if connection failed
+	 */
+	public boolean connect() {
+		//Load properties if necessary
+		if(this.dbProperties == null){
+			if(!loadDBProperties()){
+				return false;
+			}
+		}
+		
+		//Load Driver
+		System.out.println("Load jdbc Driver \"" + dbProperties.getProperty(HILTONDUBLIN_DB_DRIVER) + "\"");
+		try {
+			Class.forName(dbProperties.getProperty(HILTONDUBLIN_DB_DRIVER));
+		} catch (ClassNotFoundException e) {
+			System.out.println("Failed to Load jdbc Driver \"" + dbProperties.getProperty(HILTONDUBLIN_DB_DRIVER) + "\"");
+			e.printStackTrace();
+			return false;
+		}
+		
+		//Connect to DB
+		String url = this.dbProperties.getProperty(HILTONDUBLIN_DB_URL);
+		String username = this.dbProperties.getProperty(HILTONDUBLIN_DB_USERNAME);
+		String password = this.dbProperties.getProperty(HILTONDUBLIN_DB_PASSWORD);
+		System.out.println("Connect to Database \"" + url + "\"");
+		try {
+			this.dbConnection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			System.out.println("Failed to connect to Database \"" + url + "\"");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Disconnect from the database
+	 * @return boolean
+	 * true if disconnection was successful
+	 * false if disconnection failed
+	 */
+	public boolean disconnect(){
+		try {
+			this.dbConnection.close();
+		} catch (SQLException e) {
+			System.out.println("Failed to disconnect Database \"" + this.dbProperties.getProperty(HILTONDUBLIN_DB_NAME) + "\"");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
 	
 }
