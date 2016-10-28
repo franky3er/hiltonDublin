8d86ae5d0c8bb366fcb2d4a6705b913ba18b6bfe
Hyunjong Park <phj9205@gmail.com>
2016-10-28 02:10:37 +0200

    Write Guest Class and make onlinereservation.html(just file!)

--- hiltonDublin/src/com/hiltondublin/users/Guest.java
+++ hiltonDublin/src/com/hiltondublin/users/Guest.java
@@ -1,5 +1,29 @@
 package com.hiltondublin.users;
 
 public class Guest {
-
+	private int guestID;
+	private String address;
+	private int passportNr;
+	
+	public int getGuestID() {
+		return guestID;
+	}
+	public void setGuestID(int guestID) {
+		this.guestID = guestID;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public double getPassportNr() {
+		return passportNr;
+	}
+	public void setPassportNr(double passportNr) {
+		this.passportNr = passportNr;
+	}
+	
 }
