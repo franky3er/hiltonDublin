fd18af48aa2919ec86e2a0005f8e1ba41ae5e28b
Frank <Frank@Frank>
2016-11-01 15:51:25 +0900

    include employee attributes and getter and setter

--- hiltonDublin/src/com/hiltondublin/users/Employee.java
+++ hiltonDublin/src/com/hiltondublin/users/Employee.java
@@ -1,5 +1,26 @@
 package com.hiltondublin.users;
 
 public class Employee extends User {
+	private String username;
+	private String password;
+	private String sessionID;
 	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSessionID() {
+		return sessionID;
+	}
+	public void setSessionID(String sessionID) {
+		this.sessionID = sessionID;
+	}
 }
